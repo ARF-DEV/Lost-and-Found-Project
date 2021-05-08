fde4fc3303823284fb6f85c5a80b6450cb0fe69d
gremlinflat <fahri.novaldi91100@gmail.com>
2021-05-08 20:24:45 +0700

    PUSH TERBARU

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Barang
+
+admin.site.site_header = 'Lost-Found Admin Dashboard'
+
+
+class BarangTable(admin.ModelAdmin):
+    list_display = ["nama_barang", "jenis_barang", "status", "isSolved"]
+    list_filter = ["jenis_barang"]
+
+
 # Register your models here.
-admin.site.register(Barang)
+admin.site.register(Barang, BarangTable)
--- dashboard/models.py
+++ dashboard/models.py
@@ -4,27 +4,43 @@ from django.db import models
 
 # Model Barang
 KATEGORI = (
-    ('KATEGORI 1', 'KATEGORI 1'),
+    ('Elektronik', 'Elektronik'),
     ('KATEGORI 2', 'KATEGORI 2'),
     ('KATEGORI 3', 'KATEGORI 3'),
     ('KATEGORI 4', 'KATEGORI 4'),
     ('KATEGORI 5', 'KATEGORI 5'),
     ('KATEGORI 6', 'KATEGORI 6'),
 )
+STATUS = (
+    ('FOUND', 'FOUND'),
+    ('LOST', 'LOST'),
+)
+
+LOCATION = (
+    ('FOUND', 'FOUND'),
+    ('LOST', 'LOST'),
+)
 
 
 class Barang(models.Model):
+    class Meta:
+        db_table = 'barang'  # nama tabel
+
     nama_barang = models.CharField(max_length=20, null=True)
     jenis_barang = models.CharField(max_length=10, choices=KATEGORI, null=True)
-    isFound = models.BooleanField(default=False)
+
+    # ------------------------- punya laporan---------------------------
+    status = models.CharField(max_length=10, choices=STATUS, null=True)
     isSolved = models.BooleanField(default=False)
     deskripsi_barang = models.TextField(null=True)
+    tgl_laporan = models.DateField(auto_now_add=True, null=True)
+
+    #userid -belom
+    #adminid -belom
+    # ------------------------- punya laporan---------------------------
+
     #id_admin = models.ForeignKey(AuthUser, models.DO_NOTHING, db_column='id_admin', default='')
     #id_user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='barang', null=True, default=None)
 
     def __str__(self):
         return f'{self.nama_barang} - {self.id}'
-
-    class Meta:
-        #managed = False
-        db_table = 'barang'
