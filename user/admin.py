1a9996752ad43f4fe58f3e9d4d158cfd1f9aaaa0
gremlinflat <fahri.novaldi91100@gmail.com>
2021-05-09 15:12:10 +0700

    Adding meta class to student and staff

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User_Profile, Student
+from .models import User_Profile, Student, Staff
 # Register your models here.
 
 
 admin.site.register(User_Profile)
 admin.site.register(Student)
+admin.site.register(Staff)
--- user/models.py
+++ user/models.py
@@ -3,28 +3,38 @@ from django.contrib.auth.models import User
 from dashboard.models import Prodi
 # Create your models here.
 
+
 class User_Profile(models.Model):
     class Meta:
         db_table = 'user-profile'  # nama tabel
         verbose_name_plural = 'Daftar User Profile'
     account = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    
+
     alamat = models.CharField(max_length=200)
     no_telp = models.CharField(max_length=20, null=True)
-    
+
     image = models.ImageField(default='avatar.jpg', upload_to='Profile_images')
-    
-    prodi_id = models.ForeignKey(Prodi, on_delete=models.CASCADE, related_name='laporan', null=True)
+
+    prodi_id = models.ForeignKey(
+        Prodi, on_delete=models.CASCADE, related_name='laporan', null=True)
 
     def __str__(self):
         return f'{self.account.username}-profile'
 
 
 class Student(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="student", null=True)
-    NIM = models.CharField(max_length=20 )
+    class Meta:
+        db_table = 'student'  # nama tabel
+        verbose_name_plural = 'Daftar Mahasiswa'
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="student", null=True)
+    NIM = models.CharField(max_length=20)
+
 
 class Staff(models.Model):
+    class Meta:
+        db_table = 'staff'  # nama tabel
+        verbose_name_plural = 'Daftar Staff & Dosen'
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     NIP = models.CharField(max_length=20)
     Jabatan = models.CharField(max_length=20)
