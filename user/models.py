82506fa11c5cb1f1c1c4042a193baa31e41f3669
gremlinflat <fahri.novaldi91100@gmail.com>
2021-05-09 21:48:54 +0700

    fix bug account

--- user/models.py
+++ user/models.py
@@ -9,7 +9,7 @@ class User_Profile(models.Model):
     class Meta:
         db_table = 'user-profile'  # nama tabel
         verbose_name_plural = 'Daftar User Profile'
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    account = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
     alamat = models.CharField(max_length=200)
     no_telp = models.CharField(max_length=20, null=True)
