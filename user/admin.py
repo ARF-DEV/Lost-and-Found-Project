b43a1264d6c3da4dc1bb830c997a3aec0936d3f5
gremlinflat <fahri.novaldi91100@gmail.com>
2021-05-11 20:49:04 +0700

    arip

--- user/admin.py
+++ user/admin.py
@@ -3,6 +3,28 @@ from .models import User_Profile, Student, Staff
 # Register your models here.
 
 
+class StudentTable(admin.ModelAdmin):
+    list_display = ["id", "nama_mahasiswa", "NIM"]
+    def nama_mahasiswa(self, obj):
+        return obj.user.first_name + ' ' + obj.user.last_name
+
 admin.site.register(User_Profile)
 admin.site.register(Student)
 admin.site.register(Staff)
+class StaffTable(admin.ModelAdmin):
+    list_display = ["id", "nama_staff", "NIP","Jabatan"]
+
+    def nama_staff(self, obj):
+        return obj.user.first_name + ' ' + obj.user.last_name
+
+class UserProfileTable(admin.ModelAdmin):
+    list_display = ["id", "nama", "alamat","no_telp", 'prodi']
+    def nama(self, obj):
+        return obj.account.first_name + ' ' + obj.account.last_name
+
+    def prodi(self, obj):
+        return obj.prodi.nama_prodi
+
+admin.site.register(User_Profile, UserProfileTable)
+admin.site.register(Student, StudentTable)
+admin.site.register(Staff, StaffTable)
